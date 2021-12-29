a253333355079aac33cd727dbb48bcb9b779cbdd
alaa140101 <alaa140011@gamil.com>
2021-12-29 11:05:15 +0300

    show rating

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -50,7 +50,15 @@
                   <h1>{{ round($total,1) }}</h1>
                   <div class="rating">
                       <h3>
-                          
+                         @for ($i = 1; $i <= 5; $i++)
+                            @if ($i <= $total)
+                                <span class="fa fa-star" aria-hidden="true"></span>
+                            @elseif($i == round($total))
+                                <span class="fa fa-star-half" aria-hidden="true"></span>
+                            @else 
+                                <span class="fa fa-star-o" aria-hidden="true"></span>
+                            @endif 
+                         @endfor 
                       </h3>
                   </div>
                   <div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
