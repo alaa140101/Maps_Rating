1969cec79ebd19da35e0f3ea103c7d5a67e2604b
alaa140101 <alaa140011@gamil.com>
2022-01-09 07:36:26 +0300

    show markers for many locations

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -33,5 +33,28 @@
         </div>
       </div>
       @endif
+      <div id="mapid" style="height: 350px;"></div>
   </div>
 </x-app-layout>
+
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+<script>
+  var longitude = {!! $places->pluck('longitude') !!}
+  var latitude = {!! $places->pluck('latitude') !!}
+
+  var map = L.map('mapid');
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  
+  var markers = [];
+
+  for(var i=0; i < longitude.length; i++) {
+    markers.push(new L.marker([latitude[i], longitude[i]]).addTo(map));
+  }
+
+  var group = new L.featureGroup(markers).getBounds();
+
+  map.fitBounds([
+    group
+  ]);
+</script>
